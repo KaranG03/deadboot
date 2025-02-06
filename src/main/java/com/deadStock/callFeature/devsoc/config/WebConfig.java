c370f4bfc719d198ed65cae5ed7fb981b6899c14
KaranG03 <137709248+KaranG03@users.noreply.github.com>
2025-02-06 19:00:01 +0530

    Update WebConfig.java
    both

--- src/main/java/com/deadStock/callFeature/devsoc/config/WebConfig.java
+++ src/main/java/com/deadStock/callFeature/devsoc/config/WebConfig.java
@@ -18,7 +18,8 @@ public class WebConfig implements  WebMvcConfigurer{
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow CORS for all endpoints
-                        .allowedOrigins("http://localhost:3000") // Allowed origins
+                        .allowedOrigins("https://deadstock-by-divyansh.up.railway.app/") // Allowed origins
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow cookies/credentials
